2580fc0b0a5b7b8750eaeb65ef49772d4a712a51
Om Muddapur <ommuddapur7@gmail.com>
2024-12-27 13:36:57 +0530

    commited keys

--- board-of-studies/app/utils/supabase/client.js
+++ board-of-studies/app/utils/supabase/client.js
@@ -3,8 +3,8 @@
 import { createBrowserClient } from '@supabase/ssr';
 
 export function createClient() {
-  const NEXT_PUBLIC_SUPABASE_URL = 'https://hfzhetmxzahyecqbahhx.supabase.co';
-  const NEXT_PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmemhldG14emFoeWVjcWJhaGh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ0MzkxMDksImV4cCI6MjA1MDAxNTEwOX0.XP-Q_sc_yj2wQ35Gwrqa3pQEbw39HFhENkmHUhvR3V4';
+  process.env.NEXT_PUBLIC_SUPABASE_URL
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
   return createBrowserClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY);
 }
